cad5292a41eda1380eb56f40d01e36c9659183cd
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-10-17 20:32:53 +0100

    styling tweaks

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ function App() {
 
     return (
         <div className="App">
-            <h1 className="text-xl">HTML Parser</h1>
+            <h1 className="my-20 text-5xl">HTML Checker</h1>
             <div className="flex w-full flex-col items-center justify-center">
                 <textarea
-                    className="h-96 w-1/2 border border-black align-top"
+                    className="mb-20 h-96 w-1/2 border border-black p-2 align-top"
                     wrap="off"
                     value={html}
                     onChange={(e) => setHtml(e.target.value)}
                 />
-                <button onClick={() => checkHtml()}>CHECK HTML</button>
+                <button
+                    onClick={() => checkHtml()}
+                    className="h-20 w-40 bg-blue-200"
+                >
+                    CHECK HTML
+                </button>
             </div>
         </div>
     );
