018d82cb971ef628c4a6d605a0ac81f41db329a7
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-10-17 20:42:44 +0100

    added some checker pseudocode

--- src/utils/check-html.js
+++ src/utils/check-html.js
@@ -3,7 +3,7 @@ export default function checkHtml(html) {
 
     const currentCharInfo = {
         isTag: false,
-        isChild: false,
+        // isChild: false,
         currentTag: undefined,
         isValidTag: false,
         errorCount: 0
@@ -12,10 +12,29 @@ export default function checkHtml(html) {
     const chars = html.split("");
 
     chars.forEach((char, index) => {
+
+        // A given character in an html string can be one of the following (ignoring attributes):
+
+        // "<" - beginning of new tag
+
+        // ! Illegal when: tag is already open
+
+        // ">" - end of current tag
+
+        // ! Illegal when: tag is not already open
+
+        // "/" - closing tag indicator
+
+        // ! Illegal when: not found after a valid tag name OR not found after "<"
+
+        // "[char]" - either part of a tag, or part of the html's "child content"
+
+        // ! Illegal when: after "/" in a self closing tag while open
+
         const { isTag, isChild, currentTag, isValidTag, errorCount } = currentCharInfo;
         const isTagStart = char === "<";
         const isTagEnd = char === ">";
-        const isClosingTag === isTag && char === "/";
+        const isClosingTag = isTag && char === "/";
 
         if (!isTag && !isChild && !isTagStart) {
             errorCount++
